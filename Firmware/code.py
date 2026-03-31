988cf187d375ebf2d3566402e7778f4f5e0f4369
Fabio Larroza <fabwimm@proton.me>
2026-03-31 02:14:07 -0300

    First upload
    Uploading first project files

---
+++ Firmware/code.py
@@ -0,0 +1,53 @@
+import board
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners import DiodeConfiguration
+from kmk.keys import KC
+from kmk.modules.encoder import EncoderHandler
+from kmk.modules.layers import Layers
+from kmk.extensions.media_keys import MediaKeys
+from kmk.extensions.RGB import RGB
+from kmk.extensions.oled import OLED, OLED_DisplayMode
+
+keyboard = KMKKeyboard()
+
+keyboard.row_pins = (board.D0, board.D1, board.D2)
+keyboard.col_pins = (board.D3, board.D7, board.D8)
+keyboard.diode_orientation = DiodeConfiguration.COL2ROW
+
+encoder_handler = EncoderHandler()
+keyboard.modules.append(encoder_handler)
+encoder_handler.pins = ((board.D6, board.D9, None),)
+encoder_handler.map = [((KC.VOLU, KC.VOLD),)]
+
+keyboard.extensions.append(MediaKeys())
+
+rgb = RGB(pixel_pin=board.D10, num_pixels=9, val_default=50)
+keyboard.extensions.append(rgb)
+
+oled = OLED(
+    scl=board.SCL,
+    sda=board.SDA,
+    width=128,
+    height=32,
+    flip=False,
+    display_mode=OLED_DisplayMode.TXT
+)
+keyboard.extensions.append(oled)
+
+layers = Layers()
+keyboard.modules.append(layers)
+
+keyboard.keymap = [
+    [
+        KC.F13, KC.F14, KC.F15,
+        KC.F16, KC.F17, KC.F18,
+        KC.F19, KC.F20, KC.MUTE,
+    ],
+]
+
+oled.text("Cosmic Girl Macropad", 20, 0, 1)
+oled.text("F13-F20 | Vol Knob", 0, 16, 1)
+oled.show()
+
+if __name__ == '__main__':
+    keyboard.go()
